e9af35f568bbd43e2d96ed0045aa20498149a4fa
Quang Ngoc <21020550@vnu.edu.vn>
2022-04-18 21:57:51 +0700

    Initial Commit

---
+++ testSnake1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testSnake1
+//
+//  Created by Ngô Hán Quang Ngọc on 18/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
