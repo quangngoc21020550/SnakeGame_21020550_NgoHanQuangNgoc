1f2f2ad393bc1a420587c73610bbeab5b993d754
Quang Ngoc <21020550@vnu.edu.vn>
2022-05-05 18:24:22 +0700

    Update main.cpp

--- testSnake1/main.cpp
+++ testSnake1/main.cpp
@@ -33,6 +33,25 @@ const SDL_Color DEFAULT_COLOR = BLACK_COLOR;
 int SCREENW = 1400;
 int SCREENH = 700;
 
+struct Wall {
+    SDL_Renderer *renderer;
+    int size;
+    SDL_Rect location;
+    Wall(SDL_Window* window, SDL_Renderer* renderer_, SDL_Rect _location, int _size = 0) : renderer(renderer_) {
+        int width, height;
+        SDL_RenderGetLogicalSize(renderer, &width, &height);
+        if (width == 0 && height == 0)
+        SDL_GetWindowSize(window, &width, &height);
+        location = _location;
+        size = _size;
+    };
+    void printWall() {
+        SDL_SetRenderDrawColor(renderer,0, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &location);
+    }
+    
+};
+
 struct Apple  {
     SDL_Renderer *renderer;
     int size;
@@ -69,7 +88,8 @@ struct Snake {
     int size = 1;
     bool shield = false;
     deque<SDL_Rect> fullBody;
-    Snake(SDL_Window* window, SDL_Renderer* renderer_,SDL_Color _color, SDL_Rect _head, string _direction, int _size =1, bool _shield = false)
+    int player=1;
+    Snake(SDL_Window* window, SDL_Renderer* renderer_,SDL_Color _color, SDL_Rect _head, string _direction, int _size =1, bool _shield = false, int _player = 1)
      : renderer(renderer_)
     {
          int width, height;
@@ -81,12 +101,21 @@ struct Snake {
         size = _size;
         color = _color;
         shield = _shield;
+        player = _player;
     }
     void analyzeInput(SDL_Event &e) {
+        if(player == 2) {
                 if (e.key.keysym.sym == SDLK_UP && direction != "DOWN" ) direction = "UP";
                 if (e.key.keysym.sym == SDLK_DOWN && direction != "UP") direction =  "DOWN";
                 if (e.key.keysym.sym == SDLK_LEFT && direction != "RIGHT") direction = "LEFT";
                 if (e.key.keysym.sym == SDLK_RIGHT && direction != "LEFT") direction = "RIGHT";
+        }
+        if(player == 1) {
+                if (e.key.keysym.sym == SDLK_w && direction != "DOWN" ) direction = "UP";
+                if (e.key.keysym.sym == SDLK_s && direction != "UP") direction =  "DOWN";
+                if (e.key.keysym.sym == SDLK_a && direction != "RIGHT") direction = "LEFT";
+                if (e.key.keysym.sym == SDLK_d && direction != "LEFT") direction = "RIGHT";
+        }
     }
     void changeDir() {
 
@@ -127,10 +156,24 @@ struct Snake {
         }
     }
 
-    void checkSeflLose(){
+    void checkSeflLose(vector<Wall> aroundWall){
         if (!shield) {
             for (int i = 1 ; i < size;i++) {
                 if (head.x == fullBody[i].x && head.y == fullBody[i].y){
+                    gameRun = false;
+                }
+            }
+            for (int i = 0; i < aroundWall.size();i++) {
+                if (head.x == aroundWall[i].location.x && head.y == aroundWall[i].location.y){
+                    gameRun = false;
+                }
+            }
+        }
+    }
+    void check2v2Lose(Snake snake2) {
+        if (!shield) {
+            for (int i = 1; i < max(size, snake2.size);i++) {
+                if(head.x == snake2.fullBody[i].x && head.y == snake2.fullBody[i].y) {
                     gameRun = false;
                 }
             }
@@ -145,11 +188,12 @@ void closePrg(int &dir, bool &run , SDL_Event e);
 SDL_Texture *loadTexture(string fileName, SDL_Renderer *ren);
 void renderTexture(SDL_Renderer* ren, SDL_Texture* tex, int x, int y, int w, int h);
 void takeInput(SDL_Event &e, Snake &snake);
-void normalGame(SDL_Renderer *ren, SDL_Event &e, Snake &snake1, Apple &smallApple, Apple &shield, bool &needShield, int &timeShield);
+void normalGame(SDL_Renderer *ren, SDL_Event &e, Snake &snake1, Apple &smallApple, Apple &shield, bool &needShield, int &timeShield, Wall wall,vector<Wall> &aroundWall);
 void displayMenu(SDL_Renderer *ren,SDL_Event &e, Snake &snake, Snake &menuSnake);
 void resetSnake(Snake &snake);
 void snakeRandomWalk(Snake &snake, int dir);
-
+void pvpGame(SDL_Renderer *ren, SDL_Event &e, Snake &snake1, Snake &snake2, Apple &smallApple, Apple &shield, bool &needShield, int &timeShield);
+void createWallAround(Wall wall, vector<Wall> &aroundWall);
 
 int main(int argc, const char * argv[]) {
     // Khoi tao chuong trinh
@@ -162,19 +206,33 @@ int main(int argc, const char * argv[]) {
     string playerChoice = "";
     SDL_Event e;
     string direction = "";
+    int firstSnakeLocationX = SCREENW/2;
+    int firstSnakeLocationY = SCREENH/2;
+    int firstAppleLocationX = (SCREENW/40)*10;
+    int firstAppleLocationY = (SCREENH/40)*10;
+    int firstShieldLocationX = (SCREENW/40)*30;
+    int firstShieldLocationY = (SCREENH/40)*30;
+    int snakeSize = 10;
     int timeShield = 10000;
-    SDL_Rect head1{500,500,10,10};
-    SDL_Rect appleSmall{100,100,10,10};
-    SDL_Rect shieldHead{1000,100,10,10};
+    int sizeShield = 10;
+    SDL_Rect snakeHead{firstSnakeLocationX,firstSnakeLocationY,snakeSize,snakeSize};
     int sizeSmallApple = 10;
     int nutriSmallApple = 30;
+    SDL_Rect appleSmall{firstAppleLocationX,firstAppleLocationY,sizeSmallApple,sizeSmallApple};
+    SDL_Rect shieldHead{firstShieldLocationX,firstShieldLocationY,sizeShield,sizeShield};
+    Wall wall(win, ren ,shieldHead, sizeSmallApple);
+    vector<Wall> aroundWall;
     Apple smallApple(win, ren, appleSmall,sizeSmallApple, nutriSmallApple);
     Apple shield(win, ren, shieldHead, sizeSmallApple, nutriSmallApple);
     shield.type = "shield";
     deque<SDL_Rect> snake;
-    Snake snake1(win, ren, YELLOW_COLOR,head1, direction, true);
-    Snake menuSnake(win, ren, GREEN_COLOR, head1, direction);
+    Snake snake1(win, ren, YELLOW_COLOR,snakeHead, direction, true);
+    snake1.player = 2;
+
+    Snake menuSnake(win, ren, GREEN_COLOR, snakeHead, direction);
     bool needShield = false;
+    //System run
+    
     while (systemRun) {
     // Menu
         //menuOn = true;
@@ -185,7 +243,7 @@ int main(int argc, const char * argv[]) {
         gameRun = true;
         resetSnake(snake1);
         while (gameRun && systemRun) {
-            normalGame(ren,e,snake1,smallApple, shield, needShield, timeShield);
+            normalGame(ren, e, snake1, smallApple, shield, needShield, timeShield, wall, aroundWall );
         }
         playAgain = true;
         menuRun = true;
@@ -256,12 +314,10 @@ void takeInput(SDL_Event &e, Snake &snake) {
         }
     }
 }
-void normalGame(SDL_Renderer *ren, SDL_Event &e, Snake &snake1, Apple &smallApple, Apple &shield, bool &needShield, int &timeShield) {
+void normalGame(SDL_Renderer *ren, SDL_Event &e, Snake &snake1, Apple &smallApple, Apple &shield, bool &needShield, int &timeShield, Wall wall,vector<Wall> &aroundWall) {
     takeInput(e, snake1 );
     
-    
-    
-    
+
     snake1.changeDir();
     snake1.checkOutScreen();
     
@@ -283,11 +339,13 @@ void normalGame(SDL_Renderer *ren, SDL_Event &e, Snake &snake1, Apple &smallAppl
     SDL_RenderClear(ren);
 
     snake1.printSnake();
-    snake1.checkSeflLose();
+    createWallAround(wall, aroundWall );
+    
+    snake1.checkSeflLose(aroundWall);
     
     if(needShield) shield.printShield();
     smallApple.printApple();
-
+    
     SDL_RenderPresent(ren);
     SDL_Delay(30);
 
@@ -338,7 +396,7 @@ void displayMenu(SDL_Renderer *ren,SDL_Event &e, Snake &snake, Snake &menuSnake)
     cout << exitIconH << endl;
     cout << positionExitIconX << endl;
     cout << positionExitIconY << endl;
-    int dir = rand()%12;
+    int dir = 0;
     while (menuRun && systemRun) {
         dir = rand()%50;
         renderTexture(ren, menu, 0, 0, SCREENW, SCREENH);
@@ -357,8 +415,8 @@ void displayMenu(SDL_Renderer *ren,SDL_Event &e, Snake &snake, Snake &menuSnake)
 
 void resetSnake(Snake &snake) {
     snake.size = 1;
-    snake.head.x = 500;
-    snake.head.y = 500;
+    snake.head.x = SCREENW/2;
+    snake.head.y = SCREENH/2;
     snake.direction = "";
 }
 
@@ -378,4 +436,89 @@ void snakeRandomWalk(Snake &snake, int dir) {
     //SDL_RenderClear(ren);
 
     snake.printSnake();
+}
+
+//void pvpGame(SDL_Renderer *ren, SDL_Event &e, Snake &snake1, Snake &snake2, Apple &smallApple, Apple &shield, bool &needShield, int &timeShield) {
+//    takeInput(e, snake2);
+//    takeInput(e, snake1);
+//
+//
+//
+//
+//    snake1.changeDir();
+//    snake2.changeDir();
+//    snake1.checkOutScreen();
+//    snake2.checkOutScreen();
+//
+//
+//    if(snake1.checkApple(smallApple)) {
+//        if(1==1) {
+//            needShield = true;
+//        }
+//    }
+//    if(snake2.checkApple(smallApple)) {
+//        if(1==1) {
+//            needShield = true;
+//        }
+//    }
+//    if (snake1.checkApple(shield)){
+//        timeShield = 0;
+//        needShield = false;
+//    }
+//    if (snake2.checkApple(shield)){
+//        timeShield = 0;
+//        needShield = false;
+//    }
+//    timeShield++;
+//    snake1.addBody();
+//    snake2.addBody();
+//    if(timeShield > 50) {snake1.shield = false; snake1.color = YELLOW_COLOR;}
+//    else {snake1.color = BLUE_COLOR;}
+//    SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+//    SDL_RenderClear(ren);
+//
+//    snake1.printSnake();
+//    snake2.printSnake();
+//    snake1.checkSeflLose();
+//    snake1.check2v2Lose(snake2);
+//    snake2.checkSeflLose();
+//    snake2.check2v2Lose(snake1);
+//
+//    //if(needShield) shield.printShield();
+//    smallApple.printApple();
+//
+//    SDL_RenderPresent(ren);
+//    SDL_Delay(30);
+//}
+
+void createWallAround(Wall wall, vector<Wall> &aroundWall) {
+
+    for (int i = 0; i < SCREENW; i=i+10) {
+        //Wall wall;
+        wall.location.x = i;
+        wall.location.y = 0;
+        aroundWall.push_back(wall);
+        wall.printWall();
+    }
+    for (int i = 0; i < SCREENW; i=i+10) {
+        //Wall wall;
+        wall.location.x = i;
+        wall.location.y = SCREENH-10;
+        aroundWall.push_back(wall);
+        wall.printWall();
+    }
+    for (int i = 0; i < SCREENH;i=i+10) {
+        //Wall wall;
+        wall.location.x = 0;
+        wall.location.y = i;
+        aroundWall.push_back(wall);
+        wall.printWall();
+    }
+    for (int i = 0; i < SCREENH;i=i+10) {
+        //Wall wall;
+        wall.location.x = SCREENW-10;
+        wall.location.y = i;
+        aroundWall.push_back(wall);
+        wall.printWall();
+    }
 }
